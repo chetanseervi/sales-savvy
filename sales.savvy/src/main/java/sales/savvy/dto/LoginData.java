cd36d5b727170b3733afb24832a3232178dbd373
chetanseervi <chetanseervi@users.noreply.github.com>
2025-06-30 15:02:01 +0530

    initial commit

---
+++ sales.savvy/src/main/java/sales/savvy/dto/LoginData.java
@@ -0,0 +1,34 @@
+package sales.savvy.dto;
+
+public class LoginData {
+
+		String username;
+		String password;
+		public LoginData() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		public LoginData(String username, String password) {
+			super();
+			this.username = username;
+			this.password = password;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		@Override
+		public String toString() {
+			return "LoginData [username=" + username + ", password=" + password + "]";
+		}
+		
+		
+}
